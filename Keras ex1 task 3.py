26a60efe00e3d31c00e2b11e367bcf7204b9e336
Andrea Velásquez <andreagvj@gmail.com>
2023-12-18 02:49:21 +0100

    task 3 🐸

--- Keras ex1 task 3.py
+++ Keras ex1 task 3.py
@@ -6,22 +6,20 @@ import tensorflow as tf
 from keras.preprocessing import image
 import numpy as np
 
-NUMBERS_CLASSIF= [0,1,2,3,4,5,6,7,8,9]
-
 #make sure to pre process the image when uploading it
 def preprocess_input_image(img_path):
-    img = image.load_img(img_path, target_size=(28, 28), grayscale=True)  # Load the image and resize to 28x28 grayscale
+    img = image.load_img(img_path, target_size=(28, 28), color_mode = "grayscale")  # Load the image and resize to 28x28 grayscale
     img_array = image.img_to_array(img)
     img_array = img_array.reshape((1, 28 * 28)).astype('float32')  # Flatten the image to match the model's input shape
     # Additional preprocessing steps if required (e.g., normalization)
     return img_array
 
-img_path = r'path_to_your_image.jpg'  # Replace 'path_to_your_image.jpg' with the actual path to your image
+img_path = r'C:\Users\HP\Documents\mnist_tests\seven.jpg'  # Replace 'path_to_your_image.jpg' with the actual path to your image
 processed_image = preprocess_input_image(img_path)
 #loading the model again ok
 new_model = tf.keras.models.load_model('mnistModel.model')
 
 #making predictions based on the imported model
-predictions = new_model.predict([processed_image])
+prediction = new_model.predict([processed_image])
 
-print(np.argmax(predictions[1]))
+print(np.argmax(prediction))
